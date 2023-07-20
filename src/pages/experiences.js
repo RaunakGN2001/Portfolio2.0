ac64da0cfd842814b198742a2a1aca1a9128a96a
Raunak Gayen <rnk.express@gmail.com>
2023-07-20 14:54:09 +0530

    modifed experience page

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -7,41 +7,31 @@ import Navbar from '@/components/organisms/navbar';
 import { motion } from 'framer-motion'
 import KeyboardShortcutsMenu from '@/components/organisms/keyboardShortcutsMenu';
 
-
-
-var experienceObject = require('../data/experience.json');
-
-const nameArr = experienceObject["name"];
-const companyURLArr = experienceObject["companyURL"];
-const periodArr = experienceObject["period"];
-const roleArr = experienceObject["role"];
-const imageURLArr = experienceObject["imageURL"];
-const proofOfWorkArr = experienceObject["proofOfWork"];
-const responsibilitiesArr = experienceObject["responsibilities"];
+const { experienceArr } = require('../data/experience');
 
 const Experiences = () => {
 
   return (
     <>
-    <KeyboardShortcutsMenu />
-    <Navbar />
-    <Box as={motion.div} 
-      initial={{ x: 300, opacity: 0 }}
-    animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 300, opacity: 0 }}
-    transition={{
-      type: "ease-in-out",
-      stiffness: 260,
-      damping: 20,
-    }} fontWeight='medium' className='About-container' minHeight='90vh' paddingInline={{ base:'1.5rem', sm: '5rem', md: '10rem', lg: '15rem' }} paddingTop='7rem' display='flex' flexDirection='column' alignItems={{ md: 'center', sm: 'center', lg: '' }}>
-      <CustomHeading title={'Experiences'} marginBottom={'3rem'} />
-
-      <ExperienceCard name={nameArr[1]} companyURL={companyURLArr[1]} period={periodArr[1]} role={roleArr[1]} imageURL={imageURLArr[1]} proofOfWork={proofOfWorkArr[1]} responsibilities={responsibilitiesArr[1]} lastCard={false} />
-
-      <ExperienceCard name={nameArr[0]} companyURL={companyURLArr[0]} period={periodArr[0]} role={roleArr[0]} imageURL={imageURLArr[0]} proofOfWork={proofOfWorkArr[0]} responsibilities={responsibilitiesArr[0]} lastCard={true} />
-
-    <Footer />
-    </Box>
+      <KeyboardShortcutsMenu />
+      <Navbar />
+      <Box as={motion.div}
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 300, opacity: 0 }}
+        transition={{
+          type: "ease-in-out",
+          stiffness: 260,
+          damping: 20,
+        }} fontWeight='medium' className='About-container' minHeight='90vh' paddingInline={{ base: '1.5rem', sm: '5rem', md: '10rem', lg: '15rem' }} paddingTop='7rem' display='flex' flexDirection='column' alignItems={{ md: 'center', sm: 'center', lg: '' }}>
+        <CustomHeading title={'Experiences'} marginBottom={'3rem'} />
+        <Box>
+          {experienceArr.map(data => (
+            <ExperienceCard name={data.name} companyURL={data.companyURL} period={data.period} role={data.role} imageURL={data.imageURL} proofOfWork={data.proofOfWork} responsibilities={data.responsibilities} lastCard={false} />
+          ))}
+        </Box>
+        <Footer />
+      </Box>
     </>
   )
 }
